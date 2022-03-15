3e9db941f8b69a7af03fe773ae9a1ff68067cc9a
phucts <hooquangphuc.t4@gmail.com>
2022-03-15 14:57:09 +0700

    change db

--- medical_appointment_booking/medical_appointment_booking/Models/medicalappointmentbookingContext.cs
+++ medical_appointment_booking/medical_appointment_booking/Models/medicalappointmentbookingContext.cs
@@ -27,7 +27,7 @@ namespace medical_appointment_booking.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=mssql-71296-0.cloudclusters.net,19886;Uid=phuc;Pwd=Phuc123456;Database=medical-appointment-booking");
+                optionsBuilder.UseSqlServer("Server=mssql-71296-0.cloudclusters.net,19886;Uid=phuc;Pwd=Phuc123456;Database=appointment-booking");
             }
         }
 
